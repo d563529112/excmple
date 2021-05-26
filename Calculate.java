08f5a88fa27ede384309b9260fdf2031e2c68f4d
d563529112 <563529112@qq.com>
2021-05-26 18:49:13 +0800

    first commit

---
+++ Calculate.java
@@ -0,0 +1,19 @@
+package junitfirst2020;
+
+/**
+* 被测试类，通过Junit对此类的方法进行单元测试
+*/
+public class Calculate {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
